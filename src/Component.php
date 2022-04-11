ee60e26acd7d33287553076e23cee79d85135d42
RDanieli <rodri.merlucci@gmail.com>
2022-04-11 17:27:05 -0300

    Update Query

--- src/Component.php
+++ src/Component.php
@@ -7,15 +7,6 @@ use Sohris\Core\Component\AbstractComponent;
 use Sohris\Core\Logger;
 use Sohris\Core\Utils;
 
-class Component extends AbstractComponent<?php
-
-namespace Sohris\Mysql;
-
-use React\EventLoop\Loop;
-use Sohris\Core\Component\AbstractComponent;
-use Sohris\Core\Logger;
-use Sohris\Core\Utils;
-
 class Component extends AbstractComponent
 {
     private $module_name = "Sohris_Mysql";
@@ -46,44 +37,6 @@ class Component extends AbstractComponent
 
         Pool::createConnection();
 
-    }
-
-    public function getName(): string
-    {
-        return $this->module_name;
-    }
-}
-
-{
-    private $module_name = "Sohris_Mysql";
-
-    private $logger;
-
-    private $loop;
-
-    private $configs = array();
-
-    public function __construct()
-    {
-        $this->configs = Utils::getConfigFiles('mysql');
-        $this->loop = Loop::get();
-        $this->logger = new Logger('Mysql');
-    }
-
-    public function install()
-    {
-    }
-
-    public function start()
-    {
-    }
-
-    public function startPool()
-    {
-
-        Pool::createConnection();
-
-        Pool::startPool();
     }
 
     public function getName(): string
